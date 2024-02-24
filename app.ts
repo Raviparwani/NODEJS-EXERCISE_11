e092455ef64d917657b23e029b19968862d0dbc0
Ravi Parwani <159183341+Raviparwani@users.noreply.github.com>
2024-02-25 02:43:29 +0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-//10. Names: Store the names of a few of your friends in a array called names. 
+//11. Names: Store the names of a few of your friends in a array called names. 
 // Print each person’s name by accessing each element in the list, one at a time.
 
 
